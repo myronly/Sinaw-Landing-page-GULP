e6e437504051eb46eb62661d9cc55a69002edf46
Miron <kolomiietsnazar@gmail.com>
2022-06-11 23:26:40 +0300

    11.06.2022(8)

---
+++ js/lang.js
@@ -0,0 +1,42 @@
+const langArr = {
+  menuHome: {
+    ru: "Главная",
+    en: "Home",
+    ua: "Головна",
+  },
+  menuCourse: {
+    ru: "Курсы",
+    en: "Courses",
+    ua: "Курси",
+  },
+  menuWebinar: {
+    ru: "Вебинар",
+    en: "Webinar",
+    ua: "Вебінар",
+  },
+  menuPricing: {
+    ru: "Цены",
+    en: "Pricing",
+    ua: "Ціни",
+  },
+  menuLogin: {
+    ru: "Вход",
+    en: "Login",
+    ua: "Увійти",
+  },
+  heroTitle: {
+    ru: "Развивайте свои навыки, чтобы продвинуться по карьерной лестнице.",
+    en: "Grow Your Skills To Advance Your Career Path.",
+    ua: "Розвивайте свої навички, щоб просувати свою кар’єру.",
+  },
+  heroDescr: {
+    ru: "У нас есть кое-что большое для вас, кто хочет повысить свой уровень!",
+    en: "We have something big for you who want to level up!",
+    ua: "У нас є щось велике для вас, хто хоче підвищити рівень!",
+  },
+  heroSuggest: {
+    ru: "Предложения",
+    en: "Suggestions",
+    ua: "Пропозиції",
+  },
+};
