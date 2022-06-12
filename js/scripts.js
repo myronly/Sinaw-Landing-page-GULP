9fe16b3fa4318a9626bd8bd9576a4e54cc5709f9
Miron <kolomiietsnazar@gmail.com>
2022-06-12 14:02:41 +0300

    12.06.2022(11)

--- js/scripts.js
+++ js/scripts.js
@@ -1,89 +1,90 @@
 // Custom Scripts
 "use strict";
-// Navbar
-function burgerMenu() {
-  const burger = document.querySelector(".burger");
-  const menu = document.querySelector(".menu");
-  const body = document.querySelector("body");
-  const login = document.querySelector(".menu__login");
-  const section = document.querySelectorAll("section");
+window.addEventListener("DOMContentLoaded", () => {
+  // Navbar
+  function burgerMenu() {
+    const burger = document.querySelector(".burger");
+    const menu = document.querySelector(".menu");
+    const body = document.querySelector("body");
+    const login = document.querySelector(".menu__login");
+    const section = document.querySelectorAll("section");
 
-  burger.addEventListener("click", () => {
-    if (!menu.classList.contains("active")) {
-      menu.classList.add("active");
-      burger.classList.add("active-burger");
-      body.classList.add("locked");
-      login.classList.add("active-bottom");
-      section.forEach((sect) => {
-        sect.style.filter = "blur(3px)";
-      });
-    } else {
-      menu.classList.remove("active");
-      burger.classList.remove("active-burger");
-      body.classList.remove("locked");
-      login.classList.remove("active-bottom");
-      section.forEach((sect) => {
-        sect.style.filter = "blur(0)";
-      });
-    }
-  });
-  window.addEventListener("resize", () => {
-    if (window.innerWidth > 991.98) {
-      menu.classList.remove("active");
-      burger.classList.remove("active-burger");
-      body.classList.remove("locked");
-      login.classList.remove("active-bottom");
-      section.forEach((sect) => {
-        sect.style.filter = "blur(0)";
-      });
-    }
-  });
-}
-burgerMenu();
-
-// Select language
-const getTemplate = (data = [], placeholder, selectedId) => {
-  let hash = window.location.hash;
-  hash = hash.substring(1);
-  localStorage.setItem("language", hash);
-  let change = localStorage.getItem("language");
-  let text = placeholder ?? "placeholder не указан";
-  const allLang = ["en", "ru", "ua"];
-  const items = data.map((item) => {
-    localStorage.setItem("language Placeholder", item.value);
-    localStorage.setItem("language ID", item.id);
-    let place = localStorage.getItem("language Placeholder");
-    let id = localStorage.getItem("language ID");
-    console.log(change, place, id);
-    let cls = "";
-    function changeURLLanguage() {
-      let lang = change;
-      location.href = window.location.pathname + "#" + lang;
-      // location.reload();
-    }
-    changeURLLanguage();
-    function changeLanguage() {
-      if (!allLang.includes(hash)) {
-        location.href = window.location.pathname + "#en";
-        location.reload();
+    burger.addEventListener("click", () => {
+      if (!menu.classList.contains("active")) {
+        menu.classList.add("active");
+        burger.classList.add("active-burger");
+        body.classList.add("locked");
+        login.classList.add("active-bottom");
+        section.forEach((sect) => {
+          sect.style.filter = "blur(3px)";
+        });
+      } else {
+        menu.classList.remove("active");
+        burger.classList.remove("active-burger");
+        body.classList.remove("locked");
+        login.classList.remove("active-bottom");
+        section.forEach((sect) => {
+          sect.style.filter = "blur(0)";
+        });
       }
-      change = hash;
-      for (let key in langArr) {
-        let elem = document.querySelector(".lng-" + key);
-        if (elem) {
-          elem.innerHTML = langArr[key][hash];
+    });
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 991.98) {
+        menu.classList.remove("active");
+        burger.classList.remove("active-burger");
+        body.classList.remove("locked");
+        login.classList.remove("active-bottom");
+        section.forEach((sect) => {
+          sect.style.filter = "blur(0)";
+        });
+      }
+    });
+  }
+  burgerMenu();
+
+  // Select language
+  const getTemplate = (data = [], placeholder, selectedId) => {
+    let hash = window.location.hash;
+    hash = hash.substring(1);
+    localStorage.setItem("language", hash);
+    let change = localStorage.getItem("language");
+    let text = placeholder ?? "placeholder не указан";
+    const allLang = ["en", "ru", "ua"];
+    const items = data.map((item) => {
+      localStorage.setItem("language Placeholder", item.value);
+      localStorage.setItem("language ID", item.id);
+      let place = localStorage.getItem("language Placeholder");
+      let id = localStorage.getItem("language ID");
+      console.log(change, place, id);
+      let cls = "";
+      function changeURLLanguage() {
+        let lang = change;
+        location.href = window.location.pathname + "#" + lang;
+        // location.reload();
+      }
+      changeURLLanguage();
+      function changeLanguage() {
+        if (!allLang.includes(hash)) {
+          location.href = window.location.pathname + "#en";
+          location.reload();
+        }
+        change = hash;
+        for (let key in langArr) {
+          let elem = document.querySelector(".lng-" + key);
+          if (elem) {
+            elem.innerHTML = langArr[key][hash];
+          }
         }
       }
-    }
-    changeLanguage();
-    if (item.id === selectedId) {
-      text = item.value;
-      cls = "selected";
-    }
-    return `
+      changeLanguage();
+      if (item.id === selectedId) {
+        text = item.value;
+        cls = "selected";
+      }
+      return `
       <li class="select__item ${cls}" data-type="item" data-id="${id}" data-google-lang="${item.lang}">${place}</li>`;
-  });
-  return `
+    });
+    return `
       <input type="hidden" class="hidden__input" >
       <div class="select__backdrop" data-type="backdrop"></div>
       <div class="select__input" data-type="input">
@@ -141,168 +142,168 @@ const getTemplate = (data = [], placeholder, selectedId) => {
           </ul>
       </div>
   `;
-};
+  };
 
-class Select {
-  constructor(selector, options) {
-    this.$el = document.querySelector(selector);
-    this.options = options;
-    this.selectedId = options.selectedId;
+  class Select {
+    constructor(selector, options) {
+      this.$el = document.querySelector(selector);
+      this.options = options;
+      this.selectedId = options.selectedId;
 
-    this.render();
-    this.setup();
-  }
+      this.render();
+      this.setup();
+    }
 
-  render() {
-    const { placeholder, data } = this.options;
-    this.$el.classList.add("select");
-    this.$el.innerHTML = getTemplate(data, placeholder, this.selectedId);
-  }
-  setup() {
-    this.clickHandler = this.clickHandler.bind(this);
-    this.$el.addEventListener("click", this.clickHandler);
-    this.$arrow = this.$el.querySelectorAll('[data-type="arrow"]');
-    this.$value = this.$el.querySelector('[data-type="value"]');
-  }
-  clickHandler(event) {
-    const { type } = event.target.dataset;
-    if (type === "input") {
-      this.toggle();
-    } else if (type === "item") {
-      const id = event.target.dataset.id;
-      this.select(id);
-    } else if (type === "backdrop") {
+    render() {
+      const { placeholder, data } = this.options;
+      this.$el.classList.add("select");
+      this.$el.innerHTML = getTemplate(data, placeholder, this.selectedId);
+    }
+    setup() {
+      this.clickHandler = this.clickHandler.bind(this);
+      this.$el.addEventListener("click", this.clickHandler);
+      this.$arrow = this.$el.querySelectorAll('[data-type="arrow"]');
+      this.$value = this.$el.querySelector('[data-type="value"]');
+    }
+    clickHandler(event) {
+      const { type } = event.target.dataset;
+      if (type === "input") {
+        this.toggle();
+      } else if (type === "item") {
+        const id = event.target.dataset.id;
+        this.select(id);
+      } else if (type === "backdrop") {
+        this.close();
+      }
+    }
+
+    get isOpen() {
+      return this.$el.classList.contains("open");
+    }
+
+    get current() {
+      return this.options.data.find((item) => item.id === this.selectedId);
+    }
+
+    select(id) {
+      this.selectedId = id;
+      this.$value.textContent = this.current.value;
+
+      this.$el
+        .querySelectorAll(`[data-type="item"]`)
+        .forEach((el) => el.classList.remove("selected"));
+      this.$el.querySelector(`[data-id="${id}"]`).classList.add("selected");
+
+      this.options.onSelect ? this.options.onSelect(this.current) : null;
       this.close();
     }
-  }
 
-  get isOpen() {
-    return this.$el.classList.contains("open");
-  }
-
-  get current() {
-    return this.options.data.find((item) => item.id === this.selectedId);
-  }
-
-  select(id) {
-    this.selectedId = id;
-    this.$value.textContent = this.current.value;
-
-    this.$el
-      .querySelectorAll(`[data-type="item"]`)
-      .forEach((el) => el.classList.remove("selected"));
-    this.$el.querySelector(`[data-id="${id}"]`).classList.add("selected");
-
-    this.options.onSelect ? this.options.onSelect(this.current) : null;
-    this.close();
-  }
-
-  toggle() {
-    this.isOpen ? this.close() : this.open();
-  }
-
-  open() {
-    this.$el.classList.add("open");
-    this.$arrow.forEach((item) => {
-      item.classList.add("open");
-    });
-  }
-
-  close() {
-    this.$el.classList.remove("open");
-    this.$arrow.forEach((item) => {
-      item.classList.remove("open");
-    });
-  }
-
-  destroy() {
-    this.$el.removeEventListener("click", this.clickHandler);
-    this.$el.innerHTML = "";
-  }
-}
-const select = new Select("#select", {
-  placeholder: "Eng",
-  selectedId: 1,
-  data: [
-    { id: "1", value: "Eng", lang: "en" },
-    { id: "2", value: "Ukr", lang: "ua" },
-    { id: "3", value: "Rus", lang: "ru" },
-  ],
-  onSelect(item) {
-    const allLang = ["en", "ru", "ua"];
-    localStorage.setItem("language Placeholder", item.value);
-    localStorage.setItem("language ID", item.id);
-    const input = document.querySelector(".hidden__input");
-    input.value = item.lang;
-    console.log(input.value);
-
-    function changeURLLanguage() {
-      let lang = input.value;
-      localStorage.setItem("language", lang);
-      location.href = window.location.pathname + "#" + lang;
-      // location.reload();
+    toggle() {
+      this.isOpen ? this.close() : this.open();
     }
-    changeURLLanguage();
-    function changeLanguage() {
-      let hash = window.location.hash;
-      hash = hash.substring(1);
-      if (!allLang.includes(hash)) {
-        location.href = window.location.pathname + "#en";
-        location.reload();
+
+    open() {
+      this.$el.classList.add("open");
+      this.$arrow.forEach((item) => {
+        item.classList.add("open");
+      });
+    }
+
+    close() {
+      this.$el.classList.remove("open");
+      this.$arrow.forEach((item) => {
+        item.classList.remove("open");
+      });
+    }
+
+    destroy() {
+      this.$el.removeEventListener("click", this.clickHandler);
+      this.$el.innerHTML = "";
+    }
+  }
+  const select = new Select("#select", {
+    placeholder: "Eng",
+    selectedId: 1,
+    data: [
+      { id: "1", value: "Eng", lang: "en" },
+      { id: "2", value: "Ukr", lang: "ua" },
+      { id: "3", value: "Rus", lang: "ru" },
+    ],
+    onSelect(item) {
+      const allLang = ["en", "ru", "ua"];
+      localStorage.setItem("language Placeholder", item.value);
+      localStorage.setItem("language ID", item.id);
+      const input = document.querySelector(".hidden__input");
+      input.value = item.lang;
+      console.log(input.value);
+
+      function changeURLLanguage() {
+        let lang = input.value;
+        localStorage.setItem("language", lang);
+        location.href = window.location.pathname + "#" + lang;
+        // location.reload();
       }
-      input.value = hash;
-      for (let key in langArr) {
-        let elem = document.querySelector(".lng-" + key);
-        if (elem) {
-          elem.innerHTML = langArr[key][hash];
+      changeURLLanguage();
+      function changeLanguage() {
+        let hash = window.location.hash;
+        hash = hash.substring(1);
+        if (!allLang.includes(hash)) {
+          location.href = window.location.pathname + "#en";
+          location.reload();
+        }
+        input.value = hash;
+        for (let key in langArr) {
+          let elem = document.querySelector(".lng-" + key);
+          if (elem) {
+            elem.innerHTML = langArr[key][hash];
+          }
         }
       }
-    }
-    changeLanguage();
-  },
-});
+      changeLanguage();
+    },
+  });
 
-// Learn mouseover
+  // Learn mouseover
+  function mouseover() {
+    const listItem = document.querySelectorAll(".learn__list-item");
+    const btnHover = document.querySelectorAll(".item__bottom-btn");
+    const learnHeight = document.querySelector(".learn");
 
-function mouseover() {
-  const listItem = document.querySelectorAll(".learn__list-item");
-  const btnHover = document.querySelectorAll(".item__bottom-btn");
-  const learnHeight = document.querySelector(".learn");
+    let learnHeightClient = document.lastChild.clientWidth - 35;
 
-  let learnHeightClient = document.lastChild.clientWidth - 35;
-
-  for (let i = 0; i < listItem.length; i++) {
-    if (learnHeightClient > 633) {
-      learnHeight.style.height = learnHeight.clientHeight + "px";
-    } else {
-      learnHeight.style.height = learnHeight.clientHeight - 28 + "px";
-    }
-
-    const heightStandart =
-      listItem[i].clientHeight - btnHover[i].clientHeight - 28;
-    let height = listItem[i].clientHeight - btnHover[i].clientHeight;
-    let maxHeight = listItem[i].clientHeight + btnHover[i].clientHeight;
-    let limit = 0;
-
-    listItem[i].style.maxHeight = heightStandart + "px";
-
-    listItem[i].addEventListener("mouseover", () => {
-      if (limit === 0) {
-        listItem[i].style.maxHeight = maxHeight + "px";
-        btnHover[i].classList.add("active");
-        limit = 1;
+    for (let i = 0; i < listItem.length; i++) {
+      if (learnHeightClient > 633) {
+        learnHeight.style.height = learnHeight.clientHeight + "px";
+      } else {
+        learnHeight.style.height = `${learnHeight.clientHeight} - ${28}px`;
       }
-    });
 
-    listItem[i].addEventListener("mouseleave", () => {
-      if (limit === 1) {
-        height = heightStandart;
-        listItem[i].style.maxHeight = height + "px";
-        btnHover[i].classList.remove("active");
-        limit = 0;
-      }
-    });
+      const heightStandart =
+        listItem[i].clientHeight - btnHover[i].clientHeight - 28;
+      let height = listItem[i].clientHeight - btnHover[i].clientHeight;
+      let maxHeight = listItem[i].clientHeight + btnHover[i].clientHeight;
+      let limit = 0;
+
+      listItem[i].style.maxHeight = heightStandart + "px";
+
+      listItem[i].addEventListener("mouseover", () => {
+        if (limit === 0) {
+          listItem[i].style.maxHeight = maxHeight + "px";
+          btnHover[i].classList.add("active");
+          limit = 1;
+        }
+      });
+
+      listItem[i].addEventListener("mouseleave", () => {
+        if (limit === 1) {
+          height = heightStandart;
+          listItem[i].style.maxHeight = height + "px";
+          btnHover[i].classList.remove("active");
+          limit = 0;
+        }
+      });
+    }
   }
-}
-mouseover();
+  mouseover();
+});
 
